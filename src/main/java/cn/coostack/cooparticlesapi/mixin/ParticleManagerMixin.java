00aa4737f584fdc59b6b60b9209d6d757a0ffcb1
延皓 <harvey_husky@foxmail.com>
2025-05-23 23:23:12 +0800

    修复原版 ParticleGroup 计数bug 修复 clearParticles 时的内存泄漏

--- src/main/java/cn/coostack/cooparticlesapi/mixin/ParticleManagerMixin.java
+++ src/main/java/cn/coostack/cooparticlesapi/mixin/ParticleManagerMixin.java
@@ -3,14 +3,19 @@ package cn.coostack.cooparticlesapi.mixin;
 
 import cn.coostack.cooparticlesapi.config.APIConfigManager;
 import com.google.common.collect.EvictingQueue;
+import net.minecraft.client.particle.EmitterParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.particle.ParticleTextureSheet;
 import org.spongepowered.asm.mixin.Final;
+import net.minecraft.particle.ParticleGroup;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 import java.util.Queue;
@@ -25,6 +30,11 @@ public abstract class ParticleManagerMixin {
     @Shadow
     private Queue<Particle> newParticles;
 
+    @Shadow
+    protected abstract void addTo(ParticleGroup group, int count);
+
+    @Shadow private Queue<EmitterParticle> newEmitterParticles;
+
     @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Ljava/util/Queue;poll()Ljava/lang/Object;"))
     public Object changeMaxParticles(Queue<Object> queue) {
         // 不需要判断 EnabledParticleCountInject 了，已在注入时判断，关这个一般都是因为会注入失败，所以不用担心
@@ -38,9 +48,25 @@ public abstract class ParticleManagerMixin {
                 queue1.add(particle);
             } else {
                 // 这样驱逐队列就没用了但是可以避免内存泄漏
-                particle.markDead();
+                onEvict(particle);
             }
         }
         return null;
+    }
+
+    @Inject(method = "clearParticles", at = @At("HEAD"))
+    public void clearParticles(CallbackInfo ci) {
+        particles.values().forEach(q -> q.forEach(this::onEvict));
+        newParticles.forEach(this::onEvict);
+        newEmitterParticles.forEach(this::onEvict);
+    }
+
+    @Unique
+    private void onEvict(Particle p) {
+        // 这是原版 bug，但本模组会放大这个问题所以有必要修复一下
+        if (p.isAlive()) {
+            p.markDead();
+            p.getGroup().ifPresent(group -> addTo(group, -1));
+        }
     }
 }
